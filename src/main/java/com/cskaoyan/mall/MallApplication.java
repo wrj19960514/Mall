b1fc372e8e1690a22868a885a29313ee703b4a58
adore <adoreliuxy@gmail.com>
2019-09-30 17:41:49 +0800

    searchGoods

--- src/main/java/com/cskaoyan/mall/MallApplication.java
+++ src/main/java/com/cskaoyan/mall/MallApplication.java
@@ -6,14 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-<<<<<<< HEAD
 @MapperScan("com.cskaoyan.mall.mapper")
-=======
 @EnableTransactionManagement
-@MapperScan(basePackages = "com.cskaoyan.mall")
->>>>>>> 99812116175aec6cc1a7392a2d1d155d24a277ed
 public class MallApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MallApplication.class, args);
     }
