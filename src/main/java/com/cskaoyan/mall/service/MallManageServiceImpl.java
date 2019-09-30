95a9c0be018f5e6edaf6f7ec0448769a3af22949
RocketDar <47808126+RocketDar@users.noreply.github.com>
2019-09-30 18:11:32 +0800

    完成第一个接口

--- src/main/java/com/cskaoyan/mall/service/MallManageServiceImpl.java
+++ src/main/java/com/cskaoyan/mall/service/MallManageServiceImpl.java
@@ -3,6 +3,7 @@ package com.cskaoyan.mall.service;
 import com.cskaoyan.mall.bean.Region;
 import com.cskaoyan.mall.bean.RegionExample;
 import com.cskaoyan.mall.mapper.RegionMapper;
+import com.cskaoyan.mall.vo.mallManage.RegionListVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -20,7 +21,51 @@ public class MallManageServiceImpl implements MallManageService {
         RegionExample.Criteria criteria = example.createCriteria();
         criteria.andCodeBetween(11, 65);
         List<Region> regions = regionMapper.selectByExample(example);
-        System.out.println("regions = " + regions);
-        return regions;
+        ArrayList<RegionListVo> regionListVos = new ArrayList<>();
+        for (Region region : regions) {
+            RegionListVo vo = new RegionListVo();
+            vo.setCode(region.getCode());
+            vo.setId(region.getId());
+            vo.setType(region.getType());
+            vo.setName(region.getName());
+            vo.setChildren(getRegionList2(region.getCode()));
+            regionListVos.add(vo);
+        }
+        return regionListVos;
+    }
+
+    private List getRegionList2(Integer code) {
+        RegionExample example = new RegionExample();
+        RegionExample.Criteria criteria = example.createCriteria();
+        criteria.andCodeBetween(code * 100, code * 100 + 99);
+        List<Region> regions = regionMapper.selectByExample(example);
+        ArrayList<RegionListVo> regionListVos = new ArrayList<>();
+        for (Region region : regions) {
+            RegionListVo vo = new RegionListVo();
+            vo.setCode(region.getCode());
+            vo.setId(region.getId());
+            vo.setType(region.getType());
+            vo.setName(region.getName());
+            vo.setChildren(getRegionList3(region.getCode()));
+            regionListVos.add(vo);
+        }
+        return regionListVos;
+    }
+
+    private List getRegionList3(Integer code) {
+        RegionExample example = new RegionExample();
+        RegionExample.Criteria criteria = example.createCriteria();
+        criteria.andCodeBetween(code * 100, code * 100 + 99);
+        List<Region> regions = regionMapper.selectByExample(example);
+        ArrayList<RegionListVo> regionListVos = new ArrayList<>();
+        for (Region region : regions) {
+            RegionListVo vo = new RegionListVo();
+            vo.setCode(region.getCode());
+            vo.setId(region.getId());
+            vo.setType(region.getType());
+            vo.setName(region.getName());
+            regionListVos.add(vo);
+        }
+        return regionListVos;
     }
 }
