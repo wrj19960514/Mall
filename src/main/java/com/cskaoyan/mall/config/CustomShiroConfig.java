1ad62daed4d0a816df712e0d74c38180cf7e6ccc
wrj19960514 <296987530@qq.com>
2019-10-06 21:26:21 +0800

    Merge branch 'dev' of https://github.com/wrj19960514/Mall into dev
    # Conflicts:
    #	src/main/java/com/cskaoyan/mall/config/CustomShiroConfig.java

--- src/main/java/com/cskaoyan/mall/config/CustomShiroConfig.java
+++ src/main/java/com/cskaoyan/mall/config/CustomShiroConfig.java
@@ -4,7 +4,9 @@ import com.cskaoyan.mall.shiro.AdminRealm;
 import com.cskaoyan.mall.shiro.CustomRealmAuthenticator;
 import com.cskaoyan.mall.shiro.MallSessionManager;
 import com.cskaoyan.mall.shiro.WxRealm;
+import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.realm.Realm;
+import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
@@ -12,37 +14,45 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Properties;
 
-/**
- * @author adore
- * @date 2019/10/4 19:13
- */
 @Configuration
 public class CustomShiroConfig {
+
     /*shiroFilter*/
     @Bean
-    public ShiroFilterFactoryBean shiroFilterFactoryBean(DefaultWebSecurityManager securityManager) {
+    public ShiroFilterFactoryBean shiroFilterFactoryBean(SecurityManager securityManager){
         ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+        //如果访问url没有通过认证，会重定向到loginUrl
+        shiroFilterFactoryBean.setLoginUrl("/fail");
+        //安全控制器
         shiroFilterFactoryBean.setSecurityManager(securityManager);
-        shiroFilterFactoryBean.setLoginUrl("/");
-        //不需要认证
+        //拦截器配置
+        //请求的url
+        //anon匿名的
+        //authc认证
+        //perms权限
         HashMap<String, String> filterChainDefinitionMap = new LinkedHashMap<>();
-        filterChainDefinitionMap.put("/admin/auth/login", "anon");
-        filterChainDefinitionMap.put("/admin/auth/info", "anon");
-        filterChainDefinitionMap.put("/wx/storage/fetch/**", "anon");
-        filterChainDefinitionMap.put("/wx/auth/login", "anon");
-        filterChainDefinitionMap.put("/wx/user/index", "anon");
-         //需要进行认证
-        filterChainDefinitionMap.put("admin/**","authc");
+        filterChainDefinitionMap.put("/admin/auth/login","anon");
+        filterChainDefinitionMap.put("/wx/auth/login","anon");
+        filterChainDefinitionMap.put("/fail","anon");
+        filterChainDefinitionMap.put("/jpg/**","anon");
+
+        //认证
+        filterChainDefinitionMap.put("/admin/**","authc");
+        filterChainDefinitionMap.put("/wx/user/**","authc");
+        filterChainDefinitionMap.put("/wx/order/**","authc");
+        //filterChainDefinitionMap.put("/admin/logout","logout");
+
         shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
         return shiroFilterFactoryBean;
     }
 
-
+    /*securityManager*/
     @Bean
     public DefaultWebSecurityManager securityManager(@Qualifier("adminRealm") AdminRealm adminRealm,
                                                      @Qualifier("wxRealm") WxRealm wxRealm,
@@ -53,19 +63,28 @@ public class CustomShiroConfig {
         realms.add(wxRealm);
         securityManager.setRealms(realms);
         securityManager.setAuthenticator(customRealmAuthenticator);
+        securityManager.setSessionManager(webSessionManager());
         return securityManager;
     }
-    //注册认证器
+
     @Bean
     public CustomRealmAuthenticator customRealmAuthenticator(@Qualifier("adminRealm") AdminRealm adminRealm,
                                                              @Qualifier("wxRealm") WxRealm wxRealm){
         CustomRealmAuthenticator customRealmAuthenticator = new CustomRealmAuthenticator();
         ArrayList<Realm> realms = new ArrayList<>();
-        //告诉认证器要使用的realms是securityManager中的realms
         realms.add(adminRealm);
         realms.add(wxRealm);
         customRealmAuthenticator.setRealms(realms);
         return customRealmAuthenticator;
+    }
+    /*自定义realm*/
+
+    /*声明式使用鉴权注解的开关*/
+    @Bean
+    public AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor(SecurityManager securityManager){
+        AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor = new AuthorizationAttributeSourceAdvisor();
+        authorizationAttributeSourceAdvisor.setSecurityManager(securityManager);
+        return authorizationAttributeSourceAdvisor;
     }
 
     /*通过异常类型，映射到不同的请求上*/
