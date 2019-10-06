1b529a535ebcd4f8370c69115c1730777e501285
wrj19960514 <296987530@qq.com>
2019-10-06 16:59:55 +0800

    wrj

---
+++ src/main/java/com/cskaoyan/mall/shiro/WxRealm.java
@@ -0,0 +1,52 @@
+package com.cskaoyan.mall.shiro;
+
+import com.cskaoyan.mall.mapper.AdminMapper;
+import com.cskaoyan.mall.mapper.PermissionMapper;
+import com.cskaoyan.mall.mapper.UserMapper;
+import com.cskaoyan.mall.vo.RoleIdsVo;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class WxRealm extends AuthorizingRealm {
+
+    @Autowired
+    UserMapper userMapper;
+
+    @Autowired
+    PermissionMapper permissionMapper;
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        // username
+        String principal = (String) authenticationToken.getPrincipal();
+        String password = userMapper.queryPasswordByUsername(principal);
+        // 进行认证
+        return new SimpleAuthenticationInfo(principal,password,this.getName());
+    }
+
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        // username
+        String principal = (String) principalCollection.getPrimaryPrincipal();
+        // 授权
+        SimpleAuthorizationInfo simpleAuthorizationInfo = new SimpleAuthorizationInfo();
+        // 查询所有的身份信息
+//        RoleIdsVo roleIdsVo = adminMapper.queryRoleIdsByUsername(principal);
+        // 查询所有权限
+//        Integer[] roleIds = roleIdsVo.getRoleIds();
+//        List<String> permissions = permissionMapper.queryPermissionsByRoleIds(roleIds);
+//        simpleAuthorizationInfo.addStringPermissions(permissions);
+        return simpleAuthorizationInfo;
+    }
+}
